b57defeb39b104981166b8100c82eb1179aeaae3
Icaro Meidem <icaromeidem2@gmail.com>
2025-08-25 19:59:01 -0300

    Ajusta setup.py para instalação do pacote

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="astropack",
+    version="1.0.0",
+    packages=find_packages(),
+    author="icaromeidem",
+    description="Versão otimizada do Astropack (cordeirossauro/astropack) para filtros do J-PAS",
+    install_requires=[],
+    python_requires=">=3.7",
+)
