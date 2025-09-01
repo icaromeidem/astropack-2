8f935b173963e454a569ffc9f01fe3196e5eab41
Icaro Meidem <icaromeidem2@gmail.com>
2025-09-01 13:22:05 -0300

    update 1.0.0

--- astropack2/setup.py
+++ astropack2/setup.py
@@ -1,11 +1,9 @@
-from setuptools import setup, find_packages
-
 setup(
     name="astropack2",
     version="1.0.0",
     packages=find_packages(),
     author="icaromeidem",
-    description="Versão otimizada do Astropack (cordeirossauro/astropack) para varios modelos e surveys",
+    description="Versão otimizada do Astropack (cordeirossauro/astropack) para filtros do J-PAS",
     install_requires=[
         "numpy",
         "pandas",
